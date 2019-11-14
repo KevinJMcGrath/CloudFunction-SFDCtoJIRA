47132d484b618edac32c06f6cdd8e6b975ef9825
antiproton85 <kevinmcgr@gmail.com>
2019-11-14 15:55:09 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from sfdc_to_jira import jira_builder
+
+from flask import escape, abort
+from google.cloud import error_reporting
+
+google_error_client = error_reporting.Client()
+
+
+
+def inbound_http(request):
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json:
+        try:
+            success, result = jira_builder.create_jira_tree(request_json)
+
+            if not success:
+                abort(400, result['message'])
+
+        except Exception as ex:
+            # google_error_client.report_exception()
+            abort(500, str(ex))
+
+
+
+    else:
+        abort(400, 'Payload missing or invalid.')
